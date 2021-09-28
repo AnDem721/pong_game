26b003bc0258f4886dcdbfd46ec004579f0b1d1c
Andrzej <andem@tuta.io>
2021-09-28 15:03:47 +0200

    pong final

--- score.py
+++ score.py
@@ -8,7 +8,7 @@ from turtle import Turtle
 class Scoreboard(Turtle): 
     def __init__(self): 
         super().__init__()
-        self.color = 'white'
+        self.color('white')
         self.penup()
         self.goto(0, 0)
         self.hideturtle()
